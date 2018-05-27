5b8a5c2aab77177194f241229b59b0f74fee997b
zrdzz <17805009970@163.com>
2018-05-27 11:17:50 +0800

    popup

---
+++ src/base/popup/popup.vue
@@ -0,0 +1,62 @@
+// 弹窗组件, 传入属性为等级popLevel、内容poptext和持续时间duration, 等级为success、warnning、error, 默认持续时间是1秒
+<template>
+  <div class="popup" v-show="popText" :class="color"> 
+    <h1 class="level"> {{popLevel}} </h1>
+    <p class="text"> {{popText}} </p>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    props: {
+      popText: {
+        type: String,
+        default: ''
+      },
+      popLevel: {
+        type: String,
+        default: ''
+      },
+      duration: {
+        type: Number,
+        default: 1000
+      }
+    },
+    computed: {
+      color() {
+        switch (this.popLevel) {
+          case 'success':
+            return 'success'
+          case 'warnning':
+            return 'warnning'
+          case 'error':
+            return 'error'
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped lang='stylus' rel='stylesheet/stylus'>
+  @import "~common/stylus/variable"
+
+  .popup
+    position: fixed 
+    top: 0
+    left: 10px
+    right: 10px
+    border-bottom-left-radius: 5px
+    border-bottom-right-radius: 5px
+    background: $color-background-g
+    .level
+      margin: 10px
+      font-size: 20px
+    .text
+      margin: 10px
+  .success
+    color: $color-success
+  .warnning
+    color: $color-warnning
+  .error
+    color: $color-error
+</style>
--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="login">
-    <form class="form" @submit.prevent="submit">
+    <form class="form" @submit.prevent="submitLogin" v-show="loginForm">
       <div class="input-container border-1px">
         <input class="account border-1px" v-model="account" type="text" placeholder="账号">
       </div>
@@ -10,26 +10,56 @@
       <div class="btn-container">
         <input class="submitBtn" type="submit" value="登录">
       </div> 
+      <div class="help">
+        <span>忘记密码?</span>
+        <span @click="register">新用户注册</span>
+      </div>
     </form>
-    <div class="help">
-      <span>忘记密码?</span>
-      <span>新用户注册</span>
+    <div class="register" v-if="registerForm">
+      <form class="form" @submit.prevent="submitRegister">
+        <div class="input-container border-1px">
+          <input class="nickname border-1px" v-model="nickname" type="text" placeholder="昵称">
+        </div>
+        <div class="input-container border-1px">
+          <input class="account border-1px" v-model="account" type="text" placeholder="账号">
+        </div>
+        <div class="input-container border-1px">
+          <input class="password border-1px" v-model="password" type="text" placeholder="密码">
+        </div>
+        <div class="btn-container">
+          <input class="submitBtn" type="submit" value="注册">
+        </div> 
+      </form>  
+      <div class="return" @click="returnLogin">
+        <img class="return-img" width="30" height="30" src="../../common/images/close.png"/> 
+        <span class="return-text">返回</span>
+      </div>
     </div>
+    <popup :popLevel="popLevel" :popText="popText"></popup>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
   import axios from 'axios'
+  import Popup from 'base/popup/popup'
 
   export default {
     data() {
       return {
+        nickname: '',
         account: '',
-        password: ''
+        password: '',
+        loginForm: true,
+        registerForm: false,
+        popLevel: 'error',
+        popText: '警告'
       }
     },
+    components: {
+      Popup
+    },
     methods: {
-      submit(e) {
+      submitLogin() {
         let data = {
           account: this.account,
           password: this.password
@@ -37,6 +67,14 @@
         axios.post('/login', data).then((res) => {
           console.log(res)
         })
+      },
+      register() {
+        this.loginForm = false
+        this.registerForm = true
+      },
+      returnLogin() {
+        this.loginForm = true
+        this.registerForm = false
       }
     }
   }
@@ -52,7 +90,6 @@
     left: 0
     right: 0
     bottom: 0
-    z-index: 100
     background-image: url('login.jpg')
     background-position: center
     background-size: 200% 100%
@@ -65,13 +102,13 @@
       .input-container
         width: 80%
         border-1px($color-text-d)
-        .account, .password
+        .account, .password, .nickname
           width: 100%
           height: 50px
           outline: none
           background: transparent
           color: $color-text
-        .account::placeholder, .password::placeholder
+        .account::placeholder, .password::placeholder, .nickname::placeholder
           color: $color-text-d
       .btn-container
         width: 80%
@@ -82,11 +119,26 @@
           border-radius: 5px
           background: $color-background-b
           color: $color-text
-    .help
-      display: flex
-      width: 80%
-      margin: 5px auto
-      justify-content: space-between
-      font-size: $font-size-medium
-      color: $color-text-b
+      .help
+        display: flex
+        width: 80%
+        margin: 5px auto
+        justify-content: space-between
+        font-size: $font-size-medium
+        color: $color-text-b
+    .register
+      position: fixed
+      top: 0
+      left: 0
+      bottom: 0
+      right: 0
+      .return
+        position: absolute
+        top: 10px
+        left: 10px
+        .return-img
+          vertical-align: middle
+        .return-text
+          vertical-align: middle
+          color: $color-text
 </style>
